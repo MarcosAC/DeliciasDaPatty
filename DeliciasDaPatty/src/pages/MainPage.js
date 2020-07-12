ce23d6f8e6ce9a32b286e14f07547bee366fde8f
Marcos Aurelio <marcos.aucorrea@gmail.com>
2020-07-12 14:31:08 -0300

    Titulo da pagina principal

--- DeliciasDaPatty/src/pages/MainPage.js
+++ DeliciasDaPatty/src/pages/MainPage.js
@@ -6,26 +6,35 @@ import Card from '../components/Card';
 class MainPage extends Component {
   render () {
     return (
-      <ScrollView horizontal={true} showsHorizontalScrollIndicator={false}>
-        <Card />
-        <Card />
-        <Card />
-        <Card />
-      </ScrollView>
-      
+      <View>
+        <Text style={styles.title}>Delicias da Patty</Text>
+        <Text style={styles.subTitle}>Descubra as delicias!</Text>
+          <ScrollView horizontal={true} showsHorizontalScrollIndicator={false} style={{ backgroundColor: '#FB8A7A' }}>
+            <Card />
+            <Card />
+            <Card />
+            <Card />
+          </ScrollView>
+      </View>
     );
   }
 }
 
-const styles = StyleSheet.create({
-  
+const styles = StyleSheet.create({ 
 
-  card: {
-    borderRadius: 20,
-    shadowColor: '#DCDCDC',
-    backgroundColor: '#FF6961',
-    height: 300,
-    width: 250
+  title: {
+    fontSize: 36,
+    marginLeft: 10,
+    fontWeight: 'bold',
+    color: '#5C2D1E'
+  },
+
+  subTitle: {
+    fontSize: 24,    
+    marginLeft: 10,
+    marginBottom: 20,
+    fontWeight: 'bold',
+    color: '#5C2D1E'
   }
 });
 
