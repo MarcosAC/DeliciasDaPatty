6adf6cea5c690154e127f2be6929d980c1522fda
Marcos Aurelio <marcos.aucorrea@gmail.com>
2020-07-12 17:06:01 -0300

    Menu inferior

--- DeliciasDaPatty/src/pages/MainPage.js
+++ DeliciasDaPatty/src/pages/MainPage.js
@@ -1,16 +1,16 @@
 import React, { Component } from 'react';
-import { View, Text, ScrollView, StyleSheet, Image, FlatList } from 'react-native';
+import { View, Text, ScrollView, StyleSheet, TouchableHighlight, Image, FlatList } from 'react-native';
 
 import Card from '../components/Card';
 
 class MainPage extends Component {
   render () {
     return (
-      <View>
+      <View style={{ backgroundColor: 'white' }}>
         <Text style={styles.title}>Delicias da Patty</Text>
         <Text style={styles.subTitle}>Descubra as delicias!</Text>
 
-        <View style={{ backgroundColor: '#DEA9A5' }}>
+        <View style={{ backgroundColor: '#DEA9A5', borderBottomLeftRadius: 40 }}>
           <ScrollView horizontal={true} showsHorizontalScrollIndicator={false}>
             <Text style={styles.categorias}>Sobremesas</Text>
             <Text style={styles.categorias}>Bolos</Text>
@@ -25,7 +25,28 @@ class MainPage extends Component {
             <Card />
             <Card />
           </ScrollView>
-        </View>          
+        </View >
+          
+        <View style={{ backgroundColor: '#DEA9A5', }}>
+          <View style={{ borderTopRightRadius: 40, backgroundColor: 'white' }}>
+            <Text style={styles.categorias}>Sobremesas</Text>
+
+            <View style={{ flexDirection: 'row', justifyContent: 'space-around', }}>
+              <TouchableHighlight onPress={() => ({})} underlayColor={'#FDE1DF'} style={{ borderRadius: 60}} >
+                  <View style={styles.button1} />
+              </TouchableHighlight>
+
+              <TouchableHighlight onPress={() => ({})} underlayColor={'#FDE1DF'} style={{ borderRadius: 60}} >
+                  <View style={styles.button} />
+              </TouchableHighlight>
+
+              <TouchableHighlight onPress={() => ({})} underlayColor={'#FDE1DF'} style={{ borderRadius: 60}} >
+                  <View style={styles.button2} />
+              </TouchableHighlight>
+            </View>              
+          </View>
+        </View>
+                  
       </View>
     );
   }
@@ -49,10 +70,38 @@ const styles = StyleSheet.create({
   },
 
   categorias: {
-    fontSize: 18, 
+    fontSize: 16, 
     color: '#5C2D1E',
-    margin: 15
+    margin: 10
   },
+
+  button: {
+    backgroundColor: '#e05a5a',
+    height: 60,
+    width: 60,
+    borderRadius: 60,
+    borderWidth: 2,
+    borderColor: 'white'
+  },
+
+  button1: {
+    backgroundColor: '#559057',
+    height: 60,
+    width: 60,
+    borderRadius: 60,
+    borderWidth: 2,
+    borderColor: 'white'
+  },
+
+  button2: {
+    backgroundColor: '#DD9305',
+    height: 60,
+    width: 60,
+    borderRadius: 60,
+    borderWidth: 2,
+    borderColor: 'white'
+  },
+  
 });
 
 export default MainPage;
