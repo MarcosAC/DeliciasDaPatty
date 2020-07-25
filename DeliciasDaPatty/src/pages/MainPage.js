5da94d4accc41d05cc3b2d7120751c5cba817a04
Marcos Aurelio <marcos.aucorrea@gmail.com>
2020-07-24 23:13:43 -0300

    Styles MainPage

--- DeliciasDaPatty/src/pages/MainPage.js
+++ DeliciasDaPatty/src/pages/MainPage.js
@@ -1,87 +1,109 @@
 import React from 'react';
-import { View, Text, ScrollView, StyleSheet, TouchableHighlight, Image, FlatList } from 'react-native';
+import { View, Text, ScrollView, StyleSheet, TouchableHighlight } from 'react-native';
 
 import FontAwesome from 'react-native-vector-icons/FontAwesome';
 import FontAwesome5 from 'react-native-vector-icons/FontAwesome5';
+
 import Card from '../components/Card';
 
-
 function MainPage({ navigation }) {
-  //render() {
-    return (
-      <View style={{ height: '100%', width: '100%', backgroundColor: 'white' }}>        
-        <View style={{ height: '15%', width: '100%', paddingLeft: 15 }}>
-          <Text style={styles.title}>Delicias da Patty</Text>
-          <Text style={styles.subTitle}>Descubra as delicias!</Text>
-        </View>
+  return (
+    <View style={styles.container}>        
+      <View style={styles.containerTitle}>
+        <Text style={styles.title}>Delicias da Patty</Text>
+        <Text style={styles.subTitle}>Descubra as delicias!</Text>
+      </View>
 
-        <View  style={{ height: '70%', width: '100%', backgroundColor: '#DEA9A5', borderBottomLeftRadius: 40 }}>
-          <ScrollView horizontal={true} showsHorizontalScrollIndicator={false}>
-              <Text style={styles.categorias}>Sobremesas</Text>
-              <Text style={styles.categorias}>Bolos</Text>
-              <Text style={styles.categorias}>Bombom aberto</Text>
-              <Text style={styles.categorias}>Delicias!</Text>
-              <Text style={styles.categorias}>Delicias!</Text>
-            </ScrollView>
+      <View style={styles.containerCards}>
+        <ScrollView horizontal={true} showsHorizontalScrollIndicator={false}>
+          <Text style={styles.categorias}>Sobremesas</Text>
+          <Text style={styles.categorias}>Bolos</Text>
+          <Text style={styles.categorias}>Bombom aberto</Text>
+          <Text style={styles.categorias}>Delicias!</Text>
+          <Text style={styles.categorias}>Delicias!</Text>
+        </ScrollView>
 
-            <ScrollView horizontal={true} showsHorizontalScrollIndicator={false} >
-              <Card screenName='Detail' />
-              <Card />
-              <Card />
-              <Card />
-          </ScrollView>
-        </View>
+        <ScrollView horizontal={true} showsHorizontalScrollIndicator={false}>
+          <Card screenName='Detail' />
+          <Card />
+          <Card />
+          <Card />
+        </ScrollView>
+      </View>
 
-        <View style={{ backgroundColor: '#DEA9A5', height: '15%', width: '100%' }}>
-          <View style={{ backgroundColor: 'white', borderTopRightRadius: 40, height: '100%', width: '100%' }}>
+      <View style={styles.mainContainerMenuBottom}>
+        <View style={styles.backgroundMenuBottom}>
+          <View style={styles.containerMenuBottom}>
 
-            <View style={{ flexDirection: 'row', justifyContent: 'space-around', height: '100%', alignItems:'center' }}>
-              <View style={styles.menuBottom}>
-                <TouchableHighlight onPress={() => ({})} underlayColor={'#FDE1DF'} style={{ borderRadius: 60}} >
-                      <View style={styles.button1}>
-                        <View style={styles.iconButton}>
-                          <FontAwesome name="birthday-cake" size={18} color="white" />
-                        </View>
-                      </View>
-                  </TouchableHighlight>
-                  <Text style={styles.textMenuBottom}>Delicias</Text>
-              </View>
-
-              <View style={styles.menuBottom}>
-                <TouchableHighlight onPress={() => navigation.navigate('Detail')} underlayColor={'#FDE1DF'} style={{ borderRadius: 60}} >
-                    <View style={styles.button}>
+            <View style={styles.menuBottom}>
+              <TouchableHighlight onPress={() => ({})} underlayColor={'#FDE1DF'} style={{ borderRadius: 60}} >
+                    <View style={styles.buttonDelicias}>
                       <View style={styles.iconButton}>
-                        <FontAwesome name="heart" size={26} color="white" />
+                        <FontAwesome name="birthday-cake" size={18} color="white" />
                       </View>
                     </View>
-                </TouchableHighlight>
-              </View>
+              </TouchableHighlight>
+              <Text style={styles.textMenuBottom}>Delicias</Text>
+            </View>
 
-              <View style={styles.menuBottom}>
-                <TouchableHighlight onPress={() => ({})} underlayColor={'#FDE1DF'} style={{ borderRadius: 60}} >
-                    <View style={styles.button2}>
-                      <View style={styles.iconButton}>
-                        <FontAwesome5 name="comments" size={18} color="white" />
-                      </View>
+            <View style={styles.menuBottom}>
+              <TouchableHighlight onPress={() => navigation.navigate('Detail')} underlayColor={'#FDE1DF'} style={{ borderRadius: 60}} >
+                  <View style={styles.buttonHome}>
+                    <View style={styles.iconButton}>
+                      <FontAwesome name="heart" size={22} color="white" />
                     </View>
-                </TouchableHighlight>
-                <Text style={styles.textMenuBottom}>Pedidos</Text>
-              </View>
-            </View>            
-          </View>
+                  </View>
+              </TouchableHighlight>
+            </View>
 
+            <View style={styles.menuBottom}>
+              <TouchableHighlight onPress={() => ({})} underlayColor={'#FDE1DF'} style={{ borderRadius: 60}} >
+                  <View style={styles.buttonPedidos}>
+                    <View style={styles.iconButton}>
+                      <FontAwesome5 name="comments" size={18} color="white" />
+                    </View>
+                  </View>
+              </TouchableHighlight>
+              <Text style={styles.textMenuBottom}>Pedidos</Text>
+            </View>
+
+          </View>            
         </View>
       </View>
-    );
-  //}
+
+    </View>
+  );
 }
 
 const styles = StyleSheet.create({ 
 
-  conatiner: {
-    flex: 1
+  container: {
+    flex: 1,
+    backgroundColor: '#FFFFFF'
+  },
+
+  containerTitle: {
+    backgroundColor: '#FFFFFF',
+    flex: 1,
+    paddingLeft: 15
   },
   
+  containerCards: {
+    width: '100%',
+    backgroundColor: '#DEA9A5',    
+    borderBottomLeftRadius: 40
+  },
+
+  mainContainerMenuBottom: {
+    flex: 1,
+    backgroundColor: '#DEA9A5'
+  },
+
+  containerMenuBottom: {
+    flexDirection: 'row',
+    alignItems: 'center'
+  },
+
   title: {
     fontFamily: 'Cream Cake',
     fontSize: 48,
@@ -100,47 +122,56 @@ const styles = StyleSheet.create({
     margin: 10
   },
 
-  textMenuBottom: {
-    fontSize: 16,
-     color: '#5C2D1E'
+  backgroundMenuBottom: {
+    flex: 1,
+    backgroundColor: '#FFFFFF',
+    borderTopRightRadius: 40
+  }, 
+
+  menuBottom: {
+    flex: 1,
+    height: '100%',
+    justifyContent: 'center',
+    alignItems: 'center'
   },
 
-  button: {
+  buttonHome: {
     backgroundColor: '#e05a5a',
     height: 80,
     width: 80,
-    borderRadius: 60,
+    borderRadius: 40,
     borderWidth: 2,
     borderColor: 'white'
   },
 
-  button1: {
+  buttonDelicias: {
     backgroundColor: '#559057',
     height: 60,
     width: 60,
-    borderRadius: 60,
+    borderRadius: 30,
     borderWidth: 2,
-    borderColor: 'white'
+    borderColor: '#FFFFFF'
   },
 
-  button2: {
+  buttonPedidos: {
     backgroundColor: '#DD9305',
     height: 60,
     width: 60,
-    borderRadius: 60,
+    borderRadius: 30,
     borderWidth: 2,
     borderColor: 'white'
-  },
-
-  menuBottom: {
-    alignItems: 'center'
   },
 
   iconButton: {
     flex: 1,
     alignItems: 'center',
     justifyContent: 'center',
-    },  
+  },  
+
+  textMenuBottom: {
+    fontSize: 16,
+    color: '#5C2D1E'
+  }, 
 });
 
 export default MainPage;
