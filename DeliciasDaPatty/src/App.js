846b9675e9620beb72ad0046a47f27a346aaa34a
Marcos Aurelio <marcos.aucorrea@gmail.com>
2020-07-31 18:19:28 -0300

    FlatList

--- DeliciasDaPatty/src/App.js
+++ DeliciasDaPatty/src/App.js
@@ -1,13 +1,14 @@
 import React, { Component } from 'react';
 import { View } from 'react-native';
 
-import MainPage from './pages/MainPage';
+import ListSobremesas from './pages/ListSobremesas';
+import Navigator from './Navigator';
 
 export default class App extends Component {
   render() {
     return(
       <View>
-        <MainPage />
+        <ListSobremesas />
       </View>
     );
   }
