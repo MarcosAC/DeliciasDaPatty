cf54325f6cef0441a5a43b3ee7dce5bf131f5bea
Marcos Aurelio <marcos.aucorrea@gmail.com>
2020-07-31 19:39:30 -0300

    Lista

--- DeliciasDaPatty/index.js
+++ DeliciasDaPatty/index.js
@@ -3,8 +3,4 @@ import {name as appName} from './app.json';
 
 import App from './src/App';
 
-<<<<<<< HEAD
 AppRegistry.registerComponent(appName, () => App);
-=======
-AppRegistry.registerComponent(appName, () => App);
->>>>>>> master
--- DeliciasDaPatty/src/pages/ListSobremesas.js
+++ DeliciasDaPatty/src/pages/ListSobremesas.js
@@ -1,102 +1,48 @@
 import React from 'react';
 import { View, Text, FlatList, Image, StyleSheet } from 'react-native';
 
-const DATA = [
-    {
-        id: 1,
-        name: "Bolo de Chocolate",
-        price: "R$ 45,00"
-    },
-    {
-        id: 2,
-        name: "Bombom Aberto",
-        price: "R$ 7,00"
-    },
-    {
-        id: 3,
-        name: "Bolo Piscina com Nutella",
-        price: "R$ 45,00"
-    },
-    {
-        id: 4,
-        name: "Pavê de Leite Ninho com Morango",
-        price: "R$ 7,00"
-    },
-    {
-        id: 5,
-        name: "Romeu e Julieta",
-        price: "R$ 6,50"
-    },
-    {
-        id: 6,
-        name: "Bolo de Pote de Chocolate e Morango",
-        price: "R$ 7,00"
-    },
-    {
-        id: 7,
-        name: "Bolo de Pote de Brigadeiro",
-        price: "R$ 7,00"
-    },
-    {
-        id: 8,
-        name: "Bolo de Chocolate",
-        price: "R$ 7,00"
-    },
-    {
-        id: 9,
-        name: "Bolo de Chocolate",
-        price: "R$ 7,00"
-    },
-    {
-        id: 10,
-        name: "Bolo de Chocolate",
-        price: "R$ 7,00"
-    },
-  ];
-
-const renderItem = ({ item: s }) => {
-    return (
-        <View style={styles.container}>
-            <View style={{ flex: 1,
-                           alignItems: 'center',
-                           borderColor: 'black',
-                           //borderWidth: 1,
-                           width: 100,
-                           height: 100 }}>
-
-                <Image style={styles.image} source={require('../../assets/imgs/img_bolo.jpg')} />
-
-            </View>
-            {/* <Image style={styles.image} source={require('../../assets/imgs/img_bolo.jpg')} /> */}
-
-            <View style={styles.containerText}>
-                <Text style={styles.text}>{s.name}</Text>
-                <Text style={styles.text}>{s.price}</Text>
-                {/* <View style={{ borderWidth: 1, borderColor: 'white', marginTop: 10 }}>
-                    <Text style={styles.text}>{s.name}</Text>
-                    
-                </View>
-
-                <View style={{ borderWidth: 1, borderColor: 'blue' }}>
-                    
-                    <Text style={styles.text}>{s.price}</Text>
-                </View> */}
-                
-            </View>
-        </View>
-    );
-};
-
- /* <FlatList
-          data={DATA}
-          keyExtractor={i => `${i.id}`}
-          renderItem={renderItem}
-        /> */
-  
 function ListSobremesas() {    
     return (
-        <View style={styles.container}>
-            {/* <Image style={styles.image} source={require('../../assets/imgs/img_bolo.jpg')} /> */}
+        <View style={{flex: 1, 
+                      padding: 10,
+                      //justifyContent: 'space-between',                     
+                      //borderColor: 'blue',
+                      //borderWidth: 2,
+                      }}>
+
+            <View style={{ flexDirection: 'row',
+                           height: 130,
+                           padding: 10,
+                           alignItems: 'center',
+                           borderColor: '#FFFFFF',
+                           borderRadius: 10,
+                           borderWidth: 2,
+                           backgroundColor: '#DEA9A5'
+                        }}>
+
+                <View style={{ 
+                           width: 100,
+                           height: 100,
+                           borderColor: 'blue',
+                           borderWidth: 2,
+                           borderRadius: 50}}>
+
+                    {/* <Image style={{ 
+                                    width: '100%',
+                                    height: '100%' }} source={require('../../assets/imgs/img_bolo.jpg')} /> */}
+
+                </View>
+
+                    <View style={{ flex: 2,//width: '70%',
+                            height: 100, 
+                            borderColor: 'green',
+                            borderWidth: 1}}>
+
+                    </View>
+            </View>
+
+           
+           
         </View>
        
     );
