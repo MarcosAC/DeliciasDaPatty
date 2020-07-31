6f86271339e649c3ddebcee3dcab55392a58b467
Marcos Aurelio <marcos.aucorrea@gmail.com>
2020-07-31 18:15:37 -0300

    FlatList

--- DeliciasDaPatty/src/pages/ListSobremesas.js
+++ DeliciasDaPatty/src/pages/ListSobremesas.js
@@ -57,45 +57,90 @@ const DATA = [
 const renderItem = ({ item: s }) => {
     return (
         <View style={styles.container}>
-            <Image style={styles.image} source={require('../../assets/imgs/img_bolo.jpg')} />
+            <View style={{ flex: 1,
+                           alignItems: 'center',
+                           borderColor: 'black',
+                           //borderWidth: 1,
+                           width: 100,
+                           height: 100 }}>
+
+                <Image style={styles.image} source={require('../../assets/imgs/img_bolo.jpg')} />
+
+            </View>
+            {/* <Image style={styles.image} source={require('../../assets/imgs/img_bolo.jpg')} /> */}
 
             <View style={styles.containerText}>
                 <Text style={styles.text}>{s.name}</Text>
                 <Text style={styles.text}>{s.price}</Text>
+                {/* <View style={{ borderWidth: 1, borderColor: 'white', marginTop: 10 }}>
+                    <Text style={styles.text}>{s.name}</Text>
+                    
+                </View>
+
+                <View style={{ borderWidth: 1, borderColor: 'blue' }}>
+                    
+                    <Text style={styles.text}>{s.price}</Text>
+                </View> */}
+                
             </View>
         </View>
     );
 };
-  
-function ListSobremesas() {    
-    return (
-        <FlatList
+
+ /* <FlatList
           data={DATA}
           keyExtractor={i => `${i.id}`}
           renderItem={renderItem}
-        />
+        /> */
+  
+function ListSobremesas() {    
+    return (
+        <View style={styles.container}>
+            {/* <Image style={styles.image} source={require('../../assets/imgs/img_bolo.jpg')} /> */}
+        </View>
+       
     );
 };
 
 const styles = StyleSheet.create({
     container: {
         flex: 1,
-        flexDirection: 'row',
-        backgroundColor: '#f9c2ff',
-        marginVertical: 8,
-        marginHorizontal: 16,
-        borderRadius: 20
+        //flexDirection: 'row',
+        //marginVertical: 8,
+        //marginHorizontal: 16,
+        //padding: 10,
+        borderRadius: 15,/* 
+        shadowColor: "#A0A0A0",
+        shadowOffset: {
+            width: 0,
+            height: 1,
+        },
+        shadowOpacity: 0.51,
+        shadowRadius: 13.16,    
+        elevation: 5, */
+        backgroundColor: '#DEA9A5',
+        borderColor: 'black',
+        borderWidth: 2,
+        height: 300,
+        width: 40
     },
 
     image: {
         flex: 1,
-        height: '100%',
-        borderRadius: 20
+        height: 100,
+        width: 100,
+        borderRadius: 50,
+        borderColor: '#FFFFFF',
+        borderWidth: 2,
     },
 
     containerText: {
         flex: 2,
-        padding: 15
+        flexDirection: 'column',
+        alignContent: 'center',
+        borderWidth: 1,
+        borderColor: 'red',
+        marginLeft: 10,
     },
 
     text: {
